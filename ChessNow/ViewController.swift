9f4eb81f3f6fc439f146476828cbbceadc552899
b3rge <johannesb98@gmail.com>
2014-11-21 20:09:52 +0100

    Initial Commit

---
+++ ChessNow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChessNow
+//
+//  Created by Johannes Berge on 21/11/14.
+//  Copyright (c) 2014 Johannes Berge & Alexander Panayotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
